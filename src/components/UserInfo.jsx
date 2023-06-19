b7e750d55c331b2d8cd751a8d47df2c0ad68e579
raheltt <rteklu4@gmail.com>
2023-06-19 16:59:06 -0500

    userinfo code added

--- src/components/UserInfo.jsx
+++ src/components/UserInfo.jsx
@@ -1,10 +1,58 @@
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
-const UserInfo = () => {
+
+import PropTypes from 'prop-types'
+
+export default function UserInfo({ users }) {
   return (
-    <div>
-        <Button variant="contained">Hello World</Button>
-    </div>
-  )
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell align="right">Name</TableCell>
+            <TableCell align="right">Email</TableCell>
+            <TableCell align="right">UserType</TableCell>
+            <TableCell align="right">Action</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {users.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              {/* <TableCell align="right">{row.name}</TableCell> */}
+              <TableCell align="right">{row.email}</TableCell>
+              <TableCell align="right">{row.role}</TableCell>
+              <TableCell align="right">
+                {
+                  (row.role === "OWNER") &&
+                  <Button variant="contained">Approved</Button>}
+              </TableCell>
+
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 }
 
-export default UserInfo
+UserInfo.propTypes = {
+  users: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      email: PropTypes.string.isRequired,
+      role: PropTypes.string.isRequired
+    })
+  ).isRequired,
+};
