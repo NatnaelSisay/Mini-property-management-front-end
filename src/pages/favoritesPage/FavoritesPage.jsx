378265e2e2facd41b64eb87c22825627a3df9f24
NatnaelSisay <natyjava8@gmail.com>
2023-06-20 10:16:19 -0500

    sample favorite pages

--- src/pages/favoritesPage/FavoritesPage.jsx
+++ src/pages/favoritesPage/FavoritesPage.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import { useState } from "react";
+
+import "./FavoritesPage.css";
+import Nav from "../../components/Nav";
+import PropertyCard from "../../components/PropertyCard";
 
 const FavoritesPage = () => {
-  return (
-    <div>FavoritesPage</div>
-  )
-}
+	const [favorites, setFavorites] = useState(mockFavs);
 
-export default FavoritesPage
+	const favoriteProperties = favorites.map((property, index) => {
+		return (
+			<div key={index} className="property-card">
+				<PropertyCard key={index} />
+			</div>
+		);
+	});
+
+	return (
+		favorites && (
+			<div>
+				<Nav />
+				<div className="container flex-wrap">{favoriteProperties}</div>
+			</div>
+		)
+	);
+};
+
+const mockFavs = Array(10).fill(null);
+
+export default FavoritesPage;
